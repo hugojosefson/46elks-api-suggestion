dd76c9373da0f7b8027d11efba172d9844461fb2
Hugo Josefson <hugo@josefson.org>
2015-12-27 00:49:31 +0100

    Use PORT env variable.

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, err => {
     if (err) {
         console.error(err);
