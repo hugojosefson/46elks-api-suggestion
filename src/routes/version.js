f4570c5c6963d3b985856fd025c967017e5e1eaf
Hugo Josefson <hugo@josefson.org>
2015-12-27 01:32:50 +0100

    Revert "Save git info when deploying to heroku."
    This reverts commit 468ea7a128cce241e20d26876fde091b914b6b7f.

--- src/routes/version.js
+++ src/routes/version.js
@@ -1,14 +1,7 @@
-import gitRepoInfo from 'git-repo-info';
+import gitInfo from 'git-repo-info';
 import _ from 'lodash';
 import loadPkg from 'load-pkg';
-import jsonFile from 'jsonfile';
-import {resolve} from 'path';
-import fileExists from 'file-exists';
 
 const version = loadPkg.sync().version;
-const gitInfo = gitRepoInfo();
 
-const fileName = resolve(__dirname, '../git-info.json');
-const gitInfoJson = fileExists(fileName) && jsonFile.readFileSync(fileName, {throws: false});
-
-export default (req, res) => res.send(_.assign({version}, gitInfo.sha ? gitInfo : gitInfoJson));
+export default (req, res) => res.send(_.assign({version}, gitInfo()));
