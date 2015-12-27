7f02780f2eea792beaeff7caec58af93d59f3ddf
Hugo Josefson <hugo@josefson.org>
2015-12-27 01:08:33 +0100

    Add /version.

---
+++ src/routes/version.js
@@ -0,0 +1,7 @@
+import gitInfo from 'git-repo-info';
+import _ from 'lodash';
+import loadPkg from 'load-pkg';
+
+const version = loadPkg.sync().version;
+
+export default (req, res) => res.send(_.assign({version}, gitInfo()));
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import version from 'routes/version';
 
 const app = express();
+app.set('json spaces', 2);
+
+app.get('/version', version);
 
 const port = process.env.PORT || 3001;
 app.listen(port, err => {
