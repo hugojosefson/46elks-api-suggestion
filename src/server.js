fa801548490ee7638e79d5d567ea9b4cf52ee8de
Hugo Josefson <hugo@josefson.org>
2015-12-27 02:23:29 +0100

    Add /echo.

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.set('json spaces', 2);
 app.get('/version', version);
 app.get('/', (req, res) => res.redirect(302, 'v2'));
 app.use('/v2', v2);
+app.use('/echo', (req, res) => res.send({headers: req.headers, body: req.body}));
 
 const port = process.env.PORT || 3001;
 app.listen(port, err => {
