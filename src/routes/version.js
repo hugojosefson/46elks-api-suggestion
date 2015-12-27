468ea7a128cce241e20d26876fde091b914b6b7f
Hugo Josefson <hugo@josefson.org>
2015-12-27 01:31:18 +0100

    Save git info when deploying to heroku.

--- src/routes/version.js
+++ src/routes/version.js
@@ -1,7 +1,14 @@
-import gitInfo from 'git-repo-info';
+import gitRepoInfo from 'git-repo-info';
 import _ from 'lodash';
 import loadPkg from 'load-pkg';
+import jsonFile from 'jsonfile';
+import {resolve} from 'path';
+import fileExists from 'file-exists';
 
 const version = loadPkg.sync().version;
+const gitInfo = gitRepoInfo();
 
-export default (req, res) => res.send(_.assign({version}, gitInfo()));
+const fileName = resolve(__dirname, '../git-info.json');
+const gitInfoJson = fileExists(fileName) && jsonFile.readFileSync(fileName, {throws: false});
+
+export default (req, res) => res.send(_.assign({version}, gitInfo.sha ? gitInfo : gitInfoJson));
