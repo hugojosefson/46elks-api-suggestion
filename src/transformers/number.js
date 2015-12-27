dc468956379c5abf3baa9293ae4f81d6fe74493e
Hugo Josefson <hugo@josefson.org>
2015-12-27 04:20:12 +0100

    Use number id instead.

--- src/transformers/number.js
+++ src/transformers/number.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 import renameKey from 'utils/rename-key';
 
 export default fullUrl => number => _(number)
-    .omit(['active', 'id'])
     .thru(renameKey('voice_start', 'voice_uri'))
     .thru(renameKey('sms_url', 'sms_uri'))
     .thru(renameKey('mms_url', 'mms_uri'))
     .thru(number => _.assign({
         _links: {
-            _self: fullUrl(encodeURIComponent(number.number))
+            _self: fullUrl(encodeURIComponent(number.id))
         }
     }, number))
+    .omit(['active', 'id'])
     .value();
