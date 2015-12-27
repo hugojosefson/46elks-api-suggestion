f43e6128e6205fbfeeaf6787557c559a5123bbdf
Hugo Josefson <hugo@josefson.org>
2015-12-27 01:37:40 +0100

    Redirect / to v2.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('json spaces', 2);
 
 app.get('/version', version);
+app.get('/', (req, res) => res.redirect(302, 'v2'));
 
 const port = process.env.PORT || 3001;
 app.listen(port, err => {
