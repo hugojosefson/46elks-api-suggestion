45bf6b12a854cb16ac20f0bd5b746bbfd383948b
Hugo Josefson <hugo@josefson.org>
2015-12-29 00:44:35 +0100

    Use voice_start_uri and voice_start_action for numbers too.

--- src/transformers/number.js
+++ src/transformers/number.js
@@ -1,15 +1,29 @@
 import _ from 'lodash';
 
 import renameKey from '../utils/rename-key';
+import onlyForKey from '../utils/only-for-key';
 
 export const back = number => _(number)
+    .mapValues(onlyForKey('voice_start_action', JSON.stringify))
     .thru(renameKey('sms_uri', 'sms_url'))
     .thru(renameKey('mms_uri', 'mms_url'))
-    .thru(renameKey('voice_uri', 'voice_start'))
+    .thru(renameKey('voice_start_uri', 'voice_start'))
+    .thru(renameKey('voice_start_action', 'voice_start'))
     .value();
 
 export default number => _(number)
-    .thru(renameKey('voice_start', 'voice_uri'))
+    .map((value, key) => {
+        if (key === 'voice_start') {
+            try {
+                return ['voice_start_action', JSON.parse(value)];
+            } catch (ignored) {
+                return ['voice_start_uri', value];
+            }
+        } else {
+            return [key, value];
+        }
+    })
+    .object()
     .thru(renameKey('sms_url', 'sms_uri'))
     .thru(renameKey('mms_url', 'mms_uri'))
     .omit(['active', 'id'])
