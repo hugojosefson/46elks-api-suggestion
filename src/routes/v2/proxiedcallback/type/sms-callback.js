7d1885041972ca1e270e18272c7b5b07e56b46f6
Hugo Josefson <hugo@josefson.org>
2016-01-04 14:59:07 +0100

    Log sms-callback.

--- src/routes/v2/proxiedcallback/type/sms-callback.js
+++ src/routes/v2/proxiedcallback/type/sms-callback.js
@@ -5,7 +5,9 @@ import {responseTransformer} from '../../../../transformers/sms';
 
 export default (req, res) => {
     const {destination_uri} = req.query;
+    console.log('req.body', req.body);
     const transformed = responseTransformer(baseUri(req))(req.body);
+    console.log('responseTransformer(baseUri(req))(req.body)', transformed);
     request({
         uri: destination_uri,
         method: 'post',
