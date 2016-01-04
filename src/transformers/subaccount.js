da0c4ae685e894d258aac847f5632bb4edfef1c9
Hugo Josefson <hugo@josefson.org>
2016-01-04 16:50:37 +0100

    Convert balanaceused to balanace_used.

--- src/transformers/subaccount.js
+++ src/transformers/subaccount.js
@@ -6,10 +6,12 @@ import renameKey from '../utils/rename-key';
 
 export const requestTransformer = baseUri => subaccount => _(subaccount)
     .thru(renameKey('usage_limit', 'usagelimit'))
+    .thru(renameKey('balanace_used', 'balanaceused'))
     .value();
 
 export const responseTransformer = baseUri => subaccount => _(subaccount)
     .thru(addParentLink(baseUri + '/v2/me/subaccounts'))
     .thru(addSelfLink(baseUri + '/v2/me/subaccounts'))
     .thru(renameKey('usagelimit', 'usage_limit'))
+    .thru(renameKey('balanaceused', 'balanace_used'))
     .value();
